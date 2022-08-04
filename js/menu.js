2c0a1ef826ec5c0854d5dff5ae6c08f8ce10ffb3
gavuolo <gabs.vuolo@gmail.com>
2022-08-04 17:41:25 -0400

    fadein

--- js/menu.js
+++ js/menu.js
@@ -12,9 +12,10 @@ function menuClick(div){
     }
     div.classList.add('click-topbar')
 
-
     if(div.innerHTML === "Início"){
         mainInicio.classList.remove('hidden')
+        mainInicio.classList.add('fadeIn')
+        
 
         mainSobre.classList.add('hidden')
         mainBiomedicina.classList.add('hidden')
@@ -23,19 +24,40 @@ function menuClick(div){
     }
     if(div.innerHTML === "Sobre"){
         mainSobre.classList.remove('hidden')
-        
+        mainSobre.classList.add('fadeIn')
+
         mainInicio.classList.add('hidden')
         mainBiomedicina.classList.add('hidden')
         mainTecnologia.classList.add('hidden')
         mainContato.classList.add('hidden')
     }
-
     if(div.innerHTML === "Biomedicina"){
         mainBiomedicina.classList.remove('hidden')
+        mainBiomedicina.classList.add('fadeIn')
         
         mainInicio.classList.add('hidden')
         mainSobre.classList.add('hidden')
         mainTecnologia.classList.add('hidden')
         mainContato.classList.add('hidden')
+    }
+    if(div.innerHTML === "Tecnologia"){
+        mainTecnologia.classList.remove('hidden')
+        mainTecnologia.classList.add('fadeIn')
+        
+        mainInicio.classList.add('hidden')
+        mainSobre.classList.add('hidden')
+        mainBiomedicina.classList.add('hidden')
+        mainContato.classList.add('hidden')
+
+    }
+    if(div.innerHTML === "Contato"){
+        mainContato.classList.remove('hidden')
+        mainContato.classList.add('fadeIn')
+        
+        mainInicio.classList.add('hidden')
+        mainSobre.classList.add('hidden')
+        mainBiomedicina.classList.add('hidden')
+        mainTecnologia.classList.add('hidden')
+
     }
 }
