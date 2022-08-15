8eff9d80e90607cb90eba6f878a543eae7ca8948
gavuolo <gabs.vuolo@gmail.com>
2022-08-15 18:02:00 -0400

    Passar fotos add

--- js/script.js
+++ js/script.js
@@ -24,51 +24,72 @@ function fecharJanela(elemento){
 
 //Crescente
 function passarDireita(){
-    let img = ['01','02','03','04','05']
-    let img01 = document.querySelector('.pic-biomed-01')
-    let img02 = document.querySelector('.pic-biomed-02') 
-    let img03 = document.querySelector('.pic-biomed-03')
-    let img04 = document.querySelector('.pic-biomed-04')
-    let img05 = document.querySelector('.pic-biomed-05')
-
-    let foto = document.querySelector('.pictures')
-    let idFoto = document.querySelectorAll('.pictures img')
-    console.log(foto.classList)
-    console.log()
-
-   /*for(i = 0 ; i < idFoto.length ; i++){
-
-        if(idFoto[i].alt === 'foto 01'){
-            img01.classList.add('hidden')
-            img02.classList.remove('hidden')
-        }
-        else if(idFoto[i].alt === 'foto 02'){
-            img02.classList.add('hidden')
-            img03.classList.remove('hidden')
-        }
-        else if(idFoto[i].alt === 'foto 03'){
-            img03.classList.add('hidden')
-            img04.classList.remove('hidden')
-        }
-        else if(idFoto[i].alt === 'foto 04'){
-            img04.classList.add('hidden')
-            img05.classList.remove('hidden')
-        }
-        else if(idFoto[i].alt === 'foto 05'){
-            img05.classList.add('hidden')
-            img01.classList.remove('hidden')
-        }
-
-    }*/
+    let foto = document.querySelector('.pictures img')
     
+    if(foto.alt === 'foto 01'){
+        console.log('oi')
+            foto.src = ''
+            foto.src = `./images/photo02.jfif`
+            foto.alt = ''
+            foto.alt = `foto 02`
+        }
+    else if(foto.alt === 'foto 02'){
+            foto.src = ''
+            foto.src =`./images/photo03.jfif`
+            foto.alt = ''
+            foto.alt = `foto 03`
+        }
+        else if(foto.alt === 'foto 03'){
+            foto.src = ''
+            foto.src = `./images/photo04.jfif`
+            foto.alt = ''
+            foto.alt = `foto 04`
+        }
+        else if(foto.alt === 'foto 04'){
+            foto.src = ''
+            foto.src = `./images/photo05.jfif`
+            foto.alt = ''
+            foto.alt = `foto 05`
+        }
+        else if(foto.alt === 'foto 05'){
+            foto.src = ''
+            foto.src = `./images/photo01.jfif`
+            foto.alt = ''
+            foto.alt = `foto 01`
+        }
 }
 
 //Decrescente
 function passarEsquerda(){
-    let img01 = document.querySelector('.pic-biomed-01')
-    let img02 = document.querySelector('.pic-biomed-02') 
-    let img03 = document.querySelector('.pic-biomed-03')
-    let img04 = document.querySelector('.pic-biomed-04')
-    let img05 = document.querySelector('.pic-biomed-05')
-    console.log(img01, img02, img03, img04, img05)
+    let foto = document.querySelector('.pictures img')
+    if(foto.alt === 'foto 01'){
+            foto.src = ''
+            foto.src = `./images/photo05.jfif`
+            foto.alt = ''
+            foto.alt = `foto 05`
+        }
+    else if(foto.alt === 'foto 02'){
+            foto.src = ''
+            foto.src =`./images/photo01.jfif`
+            foto.alt = ''
+            foto.alt = `foto 01`
+        }
+        else if(foto.alt === 'foto 03'){
+            foto.src = ''
+            foto.src = `./images/photo02.jfif`
+            foto.alt = ''
+            foto.alt = `foto 02`
+        }
+        else if(foto.alt === 'foto 04'){
+            foto.src = ''
+            foto.src = `./images/photo03.jfif`
+            foto.alt = ''
+            foto.alt = `foto 03`
+        }
+        else if(foto.alt === 'foto 05'){
+            foto.src = ''
+            foto.src = `./images/photo04.jfif`
+            foto.alt = ''
+            foto.alt = `foto 04`
+        }
 }
