653787509e33b312834a6be4f11028cac3dac3f6
gavuolo <gabs.vuolo@gmail.com>
2022-08-18 23:04:07 -0400

    att

---
+++ js/biomed.js
@@ -0,0 +1,82 @@
+function verFotos(){
+    let modal = document.querySelector('.window-hidden');
+    modal.classList.remove('hidden');
+    modal.classList.add('fadeIn');
+
+}
+function fecharJanela(elemento){
+    let modal = document.querySelector('.window-hidden');
+    modal.classList.add('hidden');
+}
+
+//Crescente
+function passarDireita(){
+    let foto = document.querySelector('.pictures img')
+    
+    if(foto.alt === 'foto 01'){
+        console.log('oi')
+            foto.src = ''
+            foto.src = `./images/photo02.jfif`
+            foto.alt = ''
+            foto.alt = `foto 02`
+        }
+        else if(foto.alt === 'foto 02'){
+            foto.src = ''
+            foto.src =`./images/photo03.jfif`
+            foto.alt = ''
+            foto.alt = `foto 03`
+        }
+        else if(foto.alt === 'foto 03'){
+            foto.src = ''
+            foto.src = `./images/photo04.jfif`
+            foto.alt = ''
+            foto.alt = `foto 04`
+        }
+        else if(foto.alt === 'foto 04'){
+            foto.src = ''
+            foto.src = `./images/photo05.jfif`
+            foto.alt = ''
+            foto.alt = `foto 05`
+        }
+        else if(foto.alt === 'foto 05'){
+            foto.src = ''
+            foto.src = `./images/photo01.jfif`
+            foto.alt = ''
+            foto.alt = `foto 01`
+        }
+}
+
+//Decrescente
+function passarEsquerda(){
+    let foto = document.querySelector('.pictures img')
+    if(foto.alt === 'foto 01'){
+            foto.src = ''
+            foto.src = `./images/photo05.jfif`
+            foto.alt = ''
+            foto.alt = `foto 05`
+        }
+    else if(foto.alt === 'foto 02'){
+            foto.src = ''
+            foto.src =`./images/photo01.jfif`
+            foto.alt = ''
+            foto.alt = `foto 01`
+        }
+        else if(foto.alt === 'foto 03'){
+            foto.src = ''
+            foto.src = `./images/photo02.jfif`
+            foto.alt = ''
+            foto.alt = `foto 02`
+        }
+        else if(foto.alt === 'foto 04'){
+            foto.src = ''
+            foto.src = `./images/photo03.jfif`
+            foto.alt = ''
+            foto.alt = `foto 03`
+        }
+        else if(foto.alt === 'foto 05'){
+            foto.src = ''
+            foto.src = `./images/photo04.jfif`
+            foto.alt = ''
+            foto.alt = `foto 04`
+        }
+}
